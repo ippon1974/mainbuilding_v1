3a5612a24c7510585fba3ec0ed7b13f8fd8645bd
Vladimir <plategi@yandex.ru>
2023-09-22 21:16:15 +0300

    o

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Head from 'next/head';
+
+export default function Index(){
+    return(
+        
+        <div>
+            <Head>
+                <title>Вторичное жилье, новостройки, ипотека. Москва, Московская область.</title>
+                <meta name="viewport" content="width=device-width"/>
+                <meta name="description" content={"Вторичное жилье, новостройки, ипотека. Москва, Московская область."} />
+            </Head>
+        <h1>Вторичное жилье. Новостройки. Ипотека. Москва, Московская область.</h1>
+        <hr />
+        mainbuilding.ru
+        </div>
+    )
+}
